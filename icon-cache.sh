f4bdfc325f57fa0e207d923df12d7ae7a5a93a10
Babu Philip V M <sasha.1594@gmail.com>
2014-04-16 10:23:13 +0530

    new

---
+++ icon-cache.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Xfce &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-N &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Butterfly &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Red &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Trans &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Violet &
+exit &
+exit
+
+
