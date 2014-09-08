5967e3d972a17f107715a912224de12e49417924
sasha1594 <sasha.1594@gmail.com>
2014-09-08 11:45:47 +0530

    --New Updated Icons and removed Butterfly

--- icon-cache.sh
+++ icon-cache.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Xfce &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Butterfly &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Ubuntu &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Red &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Trans &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Violet &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-New-Green &
 exit &
 exit
 
