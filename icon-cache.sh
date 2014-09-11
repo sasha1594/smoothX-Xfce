2c7c65d214d3b85b6c293bd18bdab742bb50e7ff
sasha1594 <sasha.1594@gmail.com>
2014-09-11 17:54:30 +0530

    --Added New and Upgraded few of the Icons

--- icon-cache.sh
+++ icon-cache.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Xfce &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-N &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Brown-N &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Purple-N &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Ubuntu &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Red &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Trans &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Violet &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-New-Green &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Leather &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-Dark &
 exit &
 exit
 
