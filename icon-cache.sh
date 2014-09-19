677a3d8754a6520ec50d710824fea4fc1554db14
sasha1594 <sasha.1594@gmail.com>
2014-09-19 14:28:39 +0530

    --Updated Icons and removed unwanted icons

--- icon-cache.sh
+++ icon-cache.sh
@@ -3,14 +3,14 @@ exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Xfce &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-N &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Brown-N &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Purple-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Ubuntu &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Purple-NG &
+exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Ubuntu-NG &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Red &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Trans &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Violet &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Green-NG &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Green-N &
 exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Leather &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-Dark &
 exit
 fi
 
