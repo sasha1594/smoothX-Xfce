6e02c5c524b70652861f957a7758742d12497af9
Babu Philip <sasha.1594@gmail.com>
2014-12-31 14:15:05 +0530

    --Updated Leather folder sets  31-12-14   at 14:15  HRS

--- icon-cache.sh
+++ icon-cache.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Xfce &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Blue-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Olive-NG &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Dark-NG &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Brown-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Purple-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Purple-NG &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Ubuntu-NG &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Red &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Trans &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Violet &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Green-NG &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Green-N &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Leather &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Brown-New &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Yellow-New &
-exec= gtk-update-icon-cache -t -f ~/.icons/smoothX-Green-New &
-exit
+set -e
+# Automatically added by dh_icons
+#update icon-caches
 
+echo "  -> Updating icon cache..."
+write_cache()
+{
+if which /usr/bin/gtk-update-icon-cache >/dev/null 2>&1 ; then
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Xfce; 
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Blue-N;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Olive-NG;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Dark-NG;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Brown-N;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Purple-N;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Purple-NG;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Ubuntu-NG;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Red;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Trans;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Violet;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Green-NG;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Green-N;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Leather;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Brown-New;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Yellow-New;
+exec= gtk-update-icon-cache -q -t -f ~/.icons/smoothX-Green-New;
+exit 0
+fi
+}
 
 
 
